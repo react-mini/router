cbdf393b296e4e2bce52c78b425551a67b5c8cb9
mane936 <53373350+mane936@users.noreply.github.com>
2023-03-10 12:09:39 +0100

    Updates

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,13 +4,11 @@ import { onClickNavigate } from "../navigation/navigateToPath";
 export interface LinkPropsInterface {
   children: any;
   path: string;
-  label: string;
 }
 
 export const Link: React.FC<LinkPropsInterface> = ({
   children,
   path,
-  label,
   ...props
 }) => {
   const clickNavigate = (e: React.MouseEvent<HTMLAnchorElement>) =>
